ae7dfe4d711ab2c0ab6d14c65ace54c2e88b96e8
Jason Anderson <jas.nanders.n@gmail.com>
2021-07-03 23:11:51 -0700

    disabled username in User model, renamed content in Post model

--- models/User.js
+++ models/User.js
@@ -16,10 +16,10 @@ User.init(
             primaryKey: true,
             autoIncrement: true
         },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
+        // username: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false
+        // },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
